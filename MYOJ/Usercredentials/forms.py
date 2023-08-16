c70d4ea837f7cc7ab10eaedadae5c10bd58f63d7
DasariRishikesh <19bcs035@iiitdwd.ac.in>
2023-08-16 22:11:11 +0530

    Started New

--- MYOJ/Usercredentials/forms.py
+++
@@ -1,9 +0,0 @@
-       
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import OJUser
-
-class OJUserCreationForm(UserCreationForm):
-    class Meta:
-        model = OJUser
-        fields = ['email', 'username', 'password1', 'password2']
